a82e03c4700c4abcb2a326eef58e681dd337f98a
NQMakersupplies <Kurt@KurtSch.com.au>
2016-10-22 08:43:37 +1000

    updated effects library

--- FastLed_Effects.h
+++ FastLed_Effects.h
@@ -14,20 +14,23 @@ class FastLed_Effects
 {
   public:
     FastLed_Effects(int num_Leds);
-
-
+    
+    
     int getNumLeds();
     void setNumLeds(int num_Leds);
-    void setHue(uint8_t hue);
+    void setHue(uint8_t _hue);
+    void setSlowSpeed(uint8_t _slowSpeed);
+    void setMedSpeed(uint8_t _medSpeed);
+    void setFastSpeed(uint8_t _fastSpeed);
 
     void fillRainbow(CRGB leds[]);
 
     void addGlitter(CRGB leds[]);
     void addGlitter(CRGB leds[], fract8 chanceOfGlitter);
+    
+    void confetti(CRGB leds[]); 
 
-    void confetti(CRGB leds[]);
-
-    void sinelon(CRGB leds[]);
+    void sinelon(CRGB leds[]); 
 
     void bpm(CRGB leds[]);
     void bpm(CRGB leds[], uint8_t beatsPerMinute);
@@ -35,19 +38,24 @@ class FastLed_Effects
     void juggle(CRGB leds[]);
 
     void dotFade(CRGB leds[]);
-    void dotFade(CRGB leds[], int16_t ledPosition);
-    void dotFadeHue(CRGB leds[], int16_t ledPosition, uint8_t hue );
-    void dotFadeColour(CRGB leds[], int16_t ledPosition, CRGB colour );
+    void dotFade(CRGB leds[], uint16_t ledPosition); 
+    void dotFadeHue(CRGB leds[], uint16_t ledPosition, uint8_t hue );
+    void dotFadeColour(CRGB leds[], uint16_t ledPosition, CRGB colour );
 
-    void dotFadeColourWithSparkle(CRGB leds[], int16_t ledPosition, CRGB colour );
+    void dotFadeColourWithSparkle(CRGB leds[], uint16_t ledPosition, CRGB colour );
 
-    void dotFadeColourWithRainbowSparkle(CRGB leds[], int16_t ledPosition, CRGB colour );
+    void dotFadeColourWithRainbowSparkle(CRGB leds[], uint16_t ledPosition, CRGB colour );
 
-    void noise(CRGB leds[]);
+    void EvenSidedGeoDotFadeColourWithRainbowSparkle(CRGB leds[], uint16_t ledPosition, CRGB colour, int sides );
+
+    void EvenSidedGeoDotFadeBounceColourWithRainbowSparkle(CRGB leds[], uint16_t ledPosition, CRGB colour, int sides );
 
   private:
     int _numLeds;
     uint8_t _hue;
+    uint8_t _slowSpeed;
+    uint8_t _medSpeed;
+    uint8_t _fastSpeed;
 };
 
 #endif
